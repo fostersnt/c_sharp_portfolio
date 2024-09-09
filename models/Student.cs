78f1b59c30101cb616b4101f7c76765be8a7e025
Foster Amponsah Asante <fostersnt@gmail.com>
2024-09-09 10:10:26 +0000

    Project start-up

---
+++ models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace students_api.models
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Student name cannot be blank")]
+        public String name { get; set; }
+
+        [Required(ErrorMessage = "Age is required")]
+        public int age { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public String status { get; set; }
+
+        [Required(ErrorMessage = "Class is a required field")]
+        public String currentClass { get; set; }
+    }
+}
